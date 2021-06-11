aa3d18da6e4660f95f16b22b3001ffc843424635
“SergiyMyronov” <sergemk@entecheco.com>
2021-06-11 21:48:41 +0300

    Initial commit

---
+++ hw7_django_modelforms/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hw7_django_modelforms project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw7_django_modelforms.settings')
+
+application = get_asgi_application()
---
+++ modelforms/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.forms import ModelForm
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name + '_' + self.last_name
+
+
+class PersonForm(ModelForm):
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name', 'email']
